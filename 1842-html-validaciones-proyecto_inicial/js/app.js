b06fa0cdd43c74d4bf281c5e827b665b676abdb7
Itter Scalvenzi <itterscalvenzi15@gmail.com>
2022-10-13 11:14:00 -0300

    Creacion de mensajes personalizados

--- 1842-html-validaciones-proyecto_inicial/js/app.js
+++ 1842-html-validaciones-proyecto_inicial/js/app.js
@@ -2,7 +2,7 @@ import { validar } from "./validaciones.js";
 
 const inputs = document.querySelectorAll("input");
 
-inputs.forEach((input) =>{
+inputs.forEach(input =>{
     input.addEventListener("blur", (input) =>{
         validar(input.target);
     });
--- 1842-html-validaciones-proyecto_inicial/js/validaciones.js
+++ 1842-html-validaciones-proyecto_inicial/js/validaciones.js
@@ -3,6 +3,51 @@ export function validar(input){
     if(validadores[tipoInput]){
         validadores[tipoInput](input);
     }
+
+    if (input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML= "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML= mostrarMensajeDeError(tipoInput, input);
+    }
+}
+
+const tipoDeErrores =[
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre:{
+        valueMissing : "El campo nombre no puede estar vacio",   
+    },
+    email:{
+        valueMissing: "El campo email no puede estar vacio",
+        typeMismatch: "El correo no es válido",
+    },
+    password:{
+        valueMissing: "El campo contraseña no puede estar vacio",
+        patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra mayúscula, una minúscula, un número y no puede contener caracteres especiales.",
+    },
+    nacimiento:{
+        valueMissing: "El campo fecha no puede estar vacio",
+        customError: "Debes tener al menos 18 años de edad",
+    }
+}
+
+function mostrarMensajeDeError(tipoInput, input){
+    let mensaje = "";
+    tipoDeErrores.forEach(error =>{
+        if(input.validity[error]){
+            console.log(error);
+            console.log(mensajesDeError[tipoInput][error]);
+            mensaje = mensajesDeError[tipoInput][error];
+        }
+    });
+    return mensaje;
 }
 
 const validadores = {
